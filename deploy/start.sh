03d3e74ab6bd91f8069455f3bf6b6246cef1e470
yuanhang <yuanhang@jd.com>
2020-03-27 15:38:36 +0800

    shell

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
-docker run --name prom --rm -d -p 9090:9090 frankhang/prom-ops:1.0
-docker run --name grafana --rm -d -p 4000:4000 frankhang/grafana:1.0
-docker run --name doppler --rm -d -p 8125:8125/udp -p 8825:8825 frankhang/doppler:1.0 -L=debug
+
+#echo $#
+#echo $1
+if [ $# = 0 ]; then
+    server=true
+    client=true
+fi
+
+if [ "$1" = "server"  ]; then
+    server=true
+
+fi
+
+if [ "$1" = "client"  ]; then
+    client=true
+fi
+
+
+if [ $server ]; then
+  docker run --name prom --rm -d --network=host --add-host=host.docker.internal:127.0.0.1 frankhang/prom-ops:1.0
+  docker run --name grafana --rm -d --network=host --add-host=host.docker.internal:127.0.0.1 frankhang/grafana:1.0
+  docker run --name doppler --rm -d --network=host --add-host=host.docker.internal:127.0.0.1 frankhang/doppler:1.0
+fi
+
+if [ $client ]; then
+  docker run --name client --rm -d --network=host --add-host=host.docker.internal:127.0.0.1 frankhang/client:1.0
+fi
