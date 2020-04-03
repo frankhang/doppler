a3d25b4db54592cf414d21c81d6abf5ffcee6d1f
yuanhang <yuanhang@jd.com>
2020-04-04 00:10:58 +0800

    client

--- mock/client/main.go
+++ mock/client/main.go
@@ -5,12 +5,13 @@ import (
 	"github.com/DataDog/datadog-go/statsd"
 	"github.com/frankhang/util/errors"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
 var (
 	//bufio.NewWriterSize(p.BufReadConn, defaultWriterSize)
-	url   = flag.String("url", "localhost:8125", "host:port")
+	url = flag.String("url", "localhost:8125", "host:port")
 )
 
 func main() {
@@ -23,8 +24,7 @@ func main() {
 
 	defer statsd.Close()
 
-	statsd.Namespace = "ns1_"
-	statsd.Tags = []string{"App:UserCenter", "Env:dev"}
+	statsd.Namespace = "drht_"
 
 
 	for {
@@ -35,8 +35,8 @@ func main() {
 		err = statsd.Gauge("example_metric_Gauge", 22, nil, 1)
 		errors.MustNil(err)
 
-		err = statsd.Histogram("example_metric_Histogram", 33.33, []string{"Method:GET"}, 1)
-		errors.MustNil(err)
+		//err = statsd.Histogram("example_metric_Histogram", 33.33, []string{"Method:GET"}, 1)
+		//errors.MustNil(err)
 
 		//
 		//err = statsd.Distribution("example_metric_Distribution", 44.44, nil, 1)
@@ -48,24 +48,42 @@ func main() {
 		//err = statsd.Timing("example_metric_Timing", 222222, nil, 1)
 		//errors.MustNil(err)
 
-		err = statsd.Set("example_metric_Set", "7777", nil, 1)
-		errors.MustNil(err)
+		//err = statsd.Set("example_metric_Set", "7777", nil, 1)
+		//errors.MustNil(err)
 
-		err = statsd.Histogram("msapi", rand.Float64() * 100, []string{"Method:GET", "Path:/path/of/api1"}, 1)
-		errors.MustNil(err)
+		for i:=0; i<10;i++ {
 
-		err = statsd.Histogram("msapi", rand.Float64() * 100, []string{"Method:POST", "Path:/path/of/api1"}, 1)
-		errors.MustNil(err)
+			statsd.Tags = []string{"App:UserCenter", "Env:dev"}
+			err = statsd.Histogram("api", float64(int(rand.Float64()*1000)), []string{"Method:GET", "Path:/api/of/UserCenter/f" + strconv.Itoa(i)}, 1)
+			errors.MustNil(err)
+			err = statsd.Histogram("api", float64(int(rand.Float64()*1000)), []string{"Method:POST", "Path:/api/of/UserCenter/f" + strconv.Itoa(i)}, 1)
+			errors.MustNil(err)
 
-		err = statsd.Histogram("msapi", rand.ExpFloat64(), []string{"Method:GET", "Path:/path/of/api2"}, 1)
-		errors.MustNil(err)
-		err = statsd.Histogram("msapi", rand.ExpFloat64(), []string{"Method:POST", "Path:/path/of/api2"}, 1)
-		errors.MustNil(err)
+			statsd.Tags = []string{"App:OrgCenter", "Env:dev"}
+			err = statsd.Histogram("api", float64(int(rand.Float64()*1000)), []string{"Method:GET", "Path:/api/of/OrgCenter/f" + strconv.Itoa(i)}, 1)
+			errors.MustNil(err)
+			err = statsd.Histogram("api", float64(int(rand.Float64()*1000)), []string{"Method:POST", "Path:/api/of/OrgCenter/f" + strconv.Itoa(i)}, 1)
+			errors.MustNil(err)
+
+			statsd.Tags = []string{"App:DeviceCenter", "Env:dev"}
+			err = statsd.Histogram("api", float64(int(rand.Float64()*1000)), []string{"Method:GET", "Path:/api/of/DeviceCenter/f" + strconv.Itoa(i)}, 1)
+			errors.MustNil(err)
+			err = statsd.Histogram("api", float64(int(rand.Float64()*1000)), []string{"Method:POST", "Path:/api/of/DeviceCenter/f" + strconv.Itoa(i)}, 1)
+			errors.MustNil(err)
+
+			statsd.Tags = []string{"App:TaskCenter", "Env:dev"}
+			err = statsd.Histogram("api", float64(int(rand.Float64()*1000)), []string{"Method:GET", "Path:/api/of/TaskCenter/f" + strconv.Itoa(i)}, 1)
+			errors.MustNil(err)
+			err = statsd.Histogram("api", float64(int(rand.Float64()*1000)), []string{"Method:POST", "Path:/api/of/TaskCenter/f" + strconv.Itoa(i)}, 1)
+			errors.MustNil(err)
+
+
+		}
+
 
 		err = statsd.Flush()
 		errors.MustNil(err)
-		time.Sleep(time.Millisecond*200)
+		time.Sleep(time.Second)
 	}
-
 
 }
